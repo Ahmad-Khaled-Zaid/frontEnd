f36e45c825729ded0f55607ba703eeaac7f90fbd
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2021-09-13 18:01:33 +0300

    upload the front end

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import Header from './components/Header';
+import Books from './components/Books';
 
-class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-    }
-  }
 
-  loginHandler = (user) => {
-    this.setState({
-      user,
-    })
-  }
 
-  logoutHandler = () => {
-    this.setState({
-      user: null,
-    })
-  }
-
+export class App extends Component {
   render() {
     return (
-      <>
-        <Router>
-          <Header user={this.state.user} onLogout={this.logoutHandler} />
-          <Switch>
-            <Route exact path="/">
-              {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
-            </Route>
-            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-          </Switch>
-          <Footer />
-        </Router>
-      </>
+      <div>
+        <Header />
+        <Books/>
+        
+      </div>
     )
   }
 }
 
-export default App;
+export default App
